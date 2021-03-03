e567fe299666780d1f9ccc5c1c0cdaca4f584509
Liviu Ionescu <ilg@livius.net>
2021-03-04 00:35:26 +0200

    cmake: add xpack_process_package_version()

--- meta/micro-os-plus-posix-io-config.cmake
+++ meta/micro-os-plus-posix-io-config.cmake
@@ -18,7 +18,7 @@ endif()
 
 set(micro-os-plus-posix-io-included TRUE)
 
-message(STATUS "Including micro-os-plus-posix-io...")
+message(STATUS "Processing xPack ${PACKAGE_JSON_NAME}@${PACKAGE_JSON_VERSION}...")
 
 # -----------------------------------------------------------------------------
 # Dependencies.
